04f078cb95e0d416e2d705bcdc3571f920b43df8
DayanaCF <36211753+DayanaCF@users.noreply.github.com>
2018-04-24 07:23:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Vanilla JavaScript
+//const input = document.querySelector('input');
+//const h1 = document.querySelector('h1');
+//input.addEventListener('Keyup', () => h1.innerHTML = input.value);
+
+
+const vm = new Vue({
+	el:'main',
+	data:{
+		Usuario: 'Dayana Germán',
+		Registro: true,
+		edad: 11,
+
+
+	}
+});
